0439bc7b662e723ee758ec343ee5cacacdad375b
zikamboi <38537962+zikamboi@users.noreply.github.com>
2021-12-21 13:41:49 +0100

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,39 @@
+<template>
+<h1> TO do App<h1/>
+<form @submit.prevent = "addNewTodo">
+<label> New Todo</label>
+<input v-model = "newTodo" name="newTodo">
+<button> Add New Todo </button>
+</form>
+</template>
+
+<script>
+
+import { ref} from 'vue';
+
+export default {
+    setup() {
+        const newTodo = ref ('');
+
+        function addNewTodo() {
+            console.log(newTodo.value);
+        } 
+            return { 
+                newTodo,
+                addNewTodo, 
+        };
+    }
+}
+</script>
+
+<style>
+
+body
+{
+    font-family : sans-serif;
+    padding-top: 1em;
+    padding-bottom: 1em;
+    font-size: 2em;
+    width:80%;
+    margin: 0 auto; 
+}
